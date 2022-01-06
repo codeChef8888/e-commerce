c5c896470c00e0f692a1c30d2322f64a6da4be59
codeChef8888 <eryashre7777@gmail.com>
2022-01-06 10:04:58 +0545

    Removed Some Unwanted Files

--- eCommerce/src/main/java/my/eCommerce/repo/ProductCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package my.eCommerce.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import my.eCommerce.model.ProductCategory;
-
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
-	public ProductCategory findByCategoryName(String name);
-}
